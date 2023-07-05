4c0d452501e507b61e526d5de2dcaeea788df8f9
Olga Ivanycheva <oly3998686@yandex.ru>
2023-07-05 18:34:37 +0300

    fix error2

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -5,6 +5,8 @@ const NotFound = require('../errors/NotFoundError');
 const BadRequest = require('../errors/BadRequest');
 const ConflictError = require('../errors/ConflictError');
 
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 module.exports.getUsers = (req, res, next) => {
   User.find({})
     .then((users) => res.send(users))
@@ -117,7 +119,7 @@ module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, `${NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key'}`, { expiresIn: '7d' });
       res.send({ token });
     })
     .catch(next);
